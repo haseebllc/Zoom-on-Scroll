9b5a3e309eb82eef71d2119d286a0341ebff3af5
M.Haseeb <131981512+newgithubuser709@users.noreply.github.com>
2024-10-24 05:58:56 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+const image = document.querySelector(".zoom-image");
+
+window.addEventListener("scroll", () => {
+    const section2 = document.querySelector(".section2");
+    const section2Rect = section2.getBoundingClientRect();
+
+    const scrollPercentage =
+        (window.scrollY + window.innerHeight - section2Rect.top) /
+        section2Rect.height;
+    const scale = 1 + scrollPercentage * 0.2; // Adjust zoom amount here
+    image.style.transform = `scale(${scale})`;
+});
